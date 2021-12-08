ca76a43dadf97e18021b31c72e3b39ae968b8759
carlitosklepa <85026273+carlitosklepa@users.noreply.github.com>
2021-12-08 17:28:00 -0300

    varias modificaciones y corrección del login

--- mercado/mercado/views.py
+++ mercado/mercado/views.py
@@ -2,19 +2,12 @@ from django.shortcuts import render
 from productos.models import Producto
 
 def inicio(request):
-    productos = Producto.objects.all()
-
-    usuario = {
-        "nombre": "Carlos",
-        "apellido": "Klepacek"
-    }
-
     context = {
-        "usuario": usuario,
-        "productos": productos
+        "productos": Producto.objects.all()
     }
     return render(request, "inicio.html", context)
 
 def login(request):
-    
+    print("=============")
+    print(request.POST.get("username", None))
     return render(request, "login.html")
