cc060f50bdfb52aafc52cf4925353446f48c5e00
carlitosklepa <85026273+carlitosklepa@users.noreply.github.com>
2021-12-09 19:01:22 -0300

    varias cosas

--- mercado/app/productos/urls.py
+++ mercado/app/productos/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
 
     #Admin
     path('admin/listar/', views.ListarAdmin.as_view(), name="admin_listar"),
-    path('admin/nuevo/', views.NuevoAdmin.as_view(), name="admin_nuevo")
+    path('admin/nuevo/', views.NuevoAdmin.as_view(), name="admin_nuevo"),
+    path('admin/editar/<int:pk>/', views.EditarAdmin.as_view(), name="admin_editar")
 ]
